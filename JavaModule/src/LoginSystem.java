a3654ce2759c28f7206e84cd5dc69916ed9e1e68
bri-enright <23362901@studeentmail.ul.ie>
2024-11-15 12:55:55 +0000

    Started working on the login menu

--- JavaModule/src/LoginSystem.java
+++ JavaModule/src/LoginSystem.java
@@ -1,4 +1,32 @@
 package JavaModule.src;
 
+
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Scanner;
+
 public class LoginSystem {
+
+   private HashMap<String, String> users;
+
+   public LoginSystem() throws IOException {
+       users = CSVManager.readUsers();
+   }
+
+   public User loginFunction(){
+       Scanner scanner = new Scanner(System.in);
+
+       System.out.println("Select your role: A)dmin E)mployee H)R");
+       String command = scanner.nextLine();
+
+       if(command.equals("A")){
+
+       }else if(command.equals("E")){
+
+       }else if(command.equals("H")){
+
+       }else{
+           System.out.println("Invalid input");
+       }
+   }
 }
