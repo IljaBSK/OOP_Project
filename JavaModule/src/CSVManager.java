c21b74962e4ac709d8627303d6961a5feca2fc2e
bri-enright <23362901@studeentmail.ul.ie>
2024-11-19 20:09:58 +0000

    grrrr

--- JavaModule/src/CSVManager.java
+++ JavaModule/src/CSVManager.java
@@ -4,7 +4,7 @@ import java.io.*;
 import java.util.HashMap;
 
 public class CSVManager {
-    private static final String filename = "UserLogins.csv";
+    private static final String filename = "ValidLogins.csv";
 
 
     public static HashMap<String, User> readValidUsers() {
