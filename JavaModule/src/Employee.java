447c784f87fb9fd26dc4db31e026347562054876
bri-enright <23362901@studeentmail.ul.ie>
2024-11-20 12:21:21 +0000

    Added details to employee class and constructor

--- JavaModule/src/Employee.java
+++ JavaModule/src/Employee.java
@@ -1,17 +1,21 @@
 package JavaModule.src;
 
 public class Employee extends User {
-    private String id;
-    private String jobType;
+
+    private String name;
+    private String doB;
+    private int ppsNo;
+    private int id;
+    private String jobTitle;
     private int scalePoint;
 
-    public Employee(String username, String password, String jobType) {
+    public Employee(String username, String password, String jobType, String name, String doB, int pps, int id, String jobTitle, int scalePoint) {
         super(username, password, jobType);
     }
 
 
-    public String getJobType() {
-        return jobType;
+    public String getJobTitle() {
+        return jobTitle;
     }
 
     public int getScalePoint() {
