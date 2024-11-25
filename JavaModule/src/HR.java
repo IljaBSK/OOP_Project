d6288274bdd48b26a75ea807763dc188536f287a
colinwalsh <23363185@studentmail.ul.ie>
2024-11-25 16:30:21 +0000

    Initial version for Employee, HR, and added HR and Employee logins

--- JavaModule/src/HR.java
+++ JavaModule/src/HR.java
@@ -1,11 +1,45 @@
 package JavaModule.src;
 
+import java.util.Scanner;
+
 public class HR extends User {
 
     public HR(String username, String password, String jobType) {
         super(username, password, jobType);
     }
 
-    //public void promoteEmployee
+    public void promoteEmployee(Employee employee) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Promoting Employee: " + employee.getName());
+        System.out.println("Current job title: " + employee.getJobTitle());
+        System.out.print("Enter new job title for promotion: ");
+        String newTitle = scanner.nextLine();
 
+        employee.setPendingPromotion(newTitle);
+        System.out.println("Promotion request sent for " + employee.getName() +
+                ". Awaiting their decision upon next login.");
+    }
+
+    public void showHRMenu(Employee employee) {
+        Scanner scanner = new Scanner(System.in);
+
+        while (true) {
+            System.out.println("\nHR Menu:");
+            System.out.println("P)romote Employee");
+            System.out.println("E)xit");
+            System.out.print("Select an option: ");
+            String choice = scanner.nextLine().toUpperCase();
+
+            switch (choice) {
+                case "P":
+                    promoteEmployee(employee);
+                    break;
+                case "E":
+                    System.out.println("Exiting HR system...");
+                    return; // Exit the menu
+                default:
+                    System.out.println("Invalid option. Please try again.");
+            }
+        }
+    }
 }
